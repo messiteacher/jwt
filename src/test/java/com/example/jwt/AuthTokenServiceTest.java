676784943bb29df3c78d9ea422ab333a3cc6112d
ShinYounHo <suh2963@gmail.com>
2025-02-07 15:35:39 +0900

    jwt util getPayload

--- src/test/java/com/example/jwt/AuthTokenServiceTest.java
+++ src/test/java/com/example/jwt/AuthTokenServiceTest.java
@@ -4,8 +4,6 @@ import com.example.jwt.domain.member.member.entity.Member;
 import com.example.jwt.domain.member.member.service.AuthTokenService;
 import com.example.jwt.domain.member.member.service.MemberService;
 import com.example.jwt.standard.Util.Ut;
-import io.jsonwebtoken.Jwt;
-import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -50,12 +48,7 @@ public class AuthTokenServiceTest {
         String jwtStr = Ut.Jwt.createToken(secretKey, expireSeconds, originPayload);
         assertThat(jwtStr).isNotBlank();
 
-        Jwt<?, ?> parseJwt = Jwts.parser()
-                .verifyWith(secretKey)
-                .build()
-                .parse(jwtStr);
-
-        Map<String, Object> parsePayload = (Map<String, Object>) parseJwt.getPayload();
+        Map<String, Object> parsePayload = Ut.Jwt.getPayload(secretKey, jwtStr);
 
         assertThat(parsePayload).containsAllEntriesOf(originPayload);
     }
