fda3d567bedbeea011aafd8a5e4d264fecbcee93
ShinYounHo <suh2963@gmail.com>
2025-02-07 15:11:19 +0900

    check payload

--- src/test/java/com/example/jwt/AuthTokenServiceTest.java
+++ src/test/java/com/example/jwt/AuthTokenServiceTest.java
@@ -4,6 +4,8 @@ import com.example.jwt.domain.member.member.entity.Member;
 import com.example.jwt.domain.member.member.service.AuthTokenService;
 import com.example.jwt.domain.member.member.service.MemberService;
 import com.example.jwt.standard.Util.Ut;
+import io.jsonwebtoken.Jwt;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,7 +14,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.security.Key;
+import javax.crypto.SecretKey;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -42,12 +44,21 @@ public class AuthTokenServiceTest {
         int expireSeconds = 60 * 60 * 24 * 365;
 
         // 토큰 시크릿 키
-        Key secretKey = Keys.hmacShaKeyFor("abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890".getBytes());
+        SecretKey secretKey = Keys.hmacShaKeyFor("abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890".getBytes());
 
-        String jwt = Ut.Jwt.createToken(secretKey, expireSeconds, Map.of("name", "john", "age", 23));
+        Map<String, Object> originPayload = Map.of("name", "john", "age", 23);
 
-        assertThat(jwt).isNotBlank();
-        System.out.println("jwt = " + jwt);
+        String jwtStr = Ut.Jwt.createToken(secretKey, expireSeconds, originPayload);
+        assertThat(jwtStr).isNotBlank();
+
+        Jwt<?, ?> parseJwt = Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parse(jwtStr);
+
+        Map<String, Object> parsePayload = (Map<String, Object>) parseJwt.getPayload();
+
+        assertThat(parsePayload).containsAllEntriesOf(originPayload);
     }
 
     @Test
