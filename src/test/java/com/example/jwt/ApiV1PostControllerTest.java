7ca60e23df450cee315f446a24b243f14bd4826c
ShinYounHo <suh2963@gmail.com>
2025-02-10 15:38:40 +0900

    userDetailService securityUser apply

--- src/test/java/com/example/jwt/ApiV1PostControllerTest.java
+++ src/test/java/com/example/jwt/ApiV1PostControllerTest.java
@@ -13,6 +13,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
@@ -291,7 +292,6 @@ public class ApiV1PostControllerTest {
         return mvc
                 .perform(
                         post("/api/v1/posts")
-                                .header("Authorization", "Bearer " + apiKey)
                                 .content("""
                                         {
                                             "title": "%s",
@@ -311,6 +311,7 @@ public class ApiV1PostControllerTest {
 
     @Test
     @DisplayName("글 작성")
+    @WithUserDetails("user3")
     void write1() throws Exception {
 
         String title = "새로운 글 제목";
