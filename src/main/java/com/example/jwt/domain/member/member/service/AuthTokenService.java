a67ce235f2fc2c904e389296113e1fc8258eacad
ShinYounHo <suh2963@gmail.com>
2025-02-10 10:59:33 +0900

    custom.jwt.value

--- src/main/java/com/example/jwt/domain/member/member/service/AuthTokenService.java
+++ src/main/java/com/example/jwt/domain/member/member/service/AuthTokenService.java
@@ -2,39 +2,41 @@ package com.example.jwt.domain.member.member.service;
 
 import com.example.jwt.domain.member.member.entity.Member;
 import com.example.jwt.standard.Util.Ut;
-import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import javax.crypto.SecretKey;
 import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class AuthTokenService {
 
+    @Value("${custom.jwt.secret-key}")
+    private String keyString;
+
+    @Value("${custom.jwt.expire-seconds}")
+    private int expireSeconds;
 
     public String genAccessToken(Member member) {
 
-        int expireSeconds = 60 * 60 * 24 * 365;
-
         return Ut.Jwt.createToken(
-                Keys.hmacShaKeyFor("abcdefghijklmnopqrstuvwxyz1234567890abcdefghijklmnopqrstuvwxyz1234567890".getBytes()),
+                keyString,
                 expireSeconds,
                 Map.of("id", member.getId(), "username", member.getUsername())
         );
     }
 
-    public Map<String, Object> getPayload(SecretKey secretKey, String token) {
+    public Map<String, Object> getPayload(String token) {
 
-        Map<String, Object> payload = Ut.Jwt.getPayload(secretKey, token);
+        Map<String, Object> payload = Ut.Jwt.getPayload(keyString, token);
 
-        if (payload == null) return null;
+        if(payload == null) return null;
 
-        Number idNo = (Number) payload.get("id");
+        Number idNo = (Number)payload.get("id");
         long id = idNo.longValue();
 
-        String username = (String) payload.get("username");
+        String username = (String)payload.get("username");
 
         return Map.of("id", id, "username", username);
     }
--- src/main/java/com/example/jwt/standard/Util/Ut.java
+++ src/main/java/com/example/jwt/standard/Util/Ut.java
@@ -3,9 +3,9 @@ package com.example.jwt.standard.Util;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 
 import javax.crypto.SecretKey;
-import java.security.Key;
 import java.util.Date;
 import java.util.Map;
 
@@ -26,7 +26,10 @@ public class Ut {
     }
 
     public static class Jwt {
-        public static String createToken(Key secretKey, int expireSeconds, Map<String, Object> claims) {
+
+        public static String createToken(String keyString, int expireSeconds, Map<String, Object> claims) {
+
+            SecretKey secretKey = Keys.hmacShaKeyFor(keyString.getBytes());
 
             Date issuedAt = new Date();
             Date expiration = new Date(issuedAt.getTime() + 1000L * expireSeconds);
@@ -41,13 +44,15 @@ public class Ut {
             return jwt;
         }
 
-        public static boolean isValidToken(SecretKey secretKey, String token) {
+        public static boolean isValidToken(String keyString, String token) {
 
             try {
+                SecretKey secretKey = Keys.hmacShaKeyFor(keyString.getBytes());
                 Jwts.parser()
                         .verifyWith(secretKey)
                         .build()
                         .parse(token);
+
             } catch (Exception e) {
                 return false;
             }
@@ -55,9 +60,14 @@ public class Ut {
             return true;
         }
 
-        public static Map<String, Object> getPayload(SecretKey secretKey, String jwtStr) {
 
-            return (Map<String, Object>) Jwts.parser()
+
+        public static Map<String, Object> getPayload(String keyString, String jwtStr) {
+
+            SecretKey secretKey = Keys.hmacShaKeyFor(keyString.getBytes());
+
+            return (Map<String, Object>) Jwts
+                    .parser()
                     .verifyWith(secretKey)
                     .build()
                     .parse(jwtStr)
